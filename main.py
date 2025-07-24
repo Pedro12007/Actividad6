fa1c54fcde9a932e6fec4172ac61c97145b04c44
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-07-24 17:36:41 -0600

    Creación de menú y opción 1 de suma, promedio y positivos o negativos terminada

--- main.py
+++ main.py
@@ -36,3 +36,41 @@ def max_and_min_num(n):
 
     return max(numbers), min(numbers)
 
+while True:
+    print('Opciones:')
+    print('1. Sumar, obtener promedio, positivos y negativos')
+    print('2. Calcular el área de un triángulo')
+    print('3. Verificar si un número es par o impar')
+    print('4. Calcular el promedio de n calificaciones')
+    print('5. Ingresar n números y mostrar el mayor y el menor')
+    print('6. Salir del programa')
+
+    option = input('Ingrese una opción: ')
+    match option:
+        case '1':
+            n = int(input('Ingrese la cantidad de números que desea sumar: '))
+            result = addition(n)
+            print()
+            print(f'La suma de los números es: {result[0]}')
+            print(f'El promedio de los números sumados es: {average(result[0], n)}')
+            print(f'La cantidad de positivos es: {result[1]}')
+            print(f'La cantidad de negativos es: {result[2]}')
+            print()
+
+        case '2':
+            pass
+
+        case '3':
+            pass
+
+        case '4':
+            pass
+
+        case '5':
+            pass
+
+        case '6':
+            break
+
+        case _:
+            print('Opción inválida. Intente nuevamente.')
